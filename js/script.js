4bbd61c1d00195b151b9127c463c445954aa56ba
Marta Nara <marta.kos1230@gmail.com>
2021-09-30 10:40:32 +0200

    Add player logic to script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,32 @@ if(randomNumber == 1){
 } else {
     computerMove = 'papier';
 }
+
+console.log('Komputer wylosował: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = '';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3'){
+    playerMove = 'nożyce';
+} else {
+    playerMove = 'niepoprawny ruch';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+printMessage('Przeciwnik wybrał: ' + computerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier' || computerMove == 'papier' && playerMove == 'nożyce' || computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  } else if (computerMove == 'kamień' && playerMove == 'kamień' || computerMove == 'papier' && playerMove == 'papier' || computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis!');
+  } else {
+    printMessage('Przegrywasz!');      
+  }
