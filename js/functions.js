4fd3bceb21ac5b99a5f449a9cfc163ee37ef4457
Marta Nara <marta.kos1230@gmail.com>
2021-10-08 11:30:00 +0200

    Change some 'let' and fucntions to 'const' in js

--- js/functions.js
+++ js/functions.js
@@ -1,55 +1,58 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-function printResult(result){
-	let div = document.createElement('div');
-	div.innerHTML = result;
-	document.getElementById('result').appendChild(div);
-}
-
-function clearResult(){
-	document.getElementById('result').innerHTML = '';
-}
-
-function getMoveName(argMoveId){
-    if(argMoveId == 1){
-    return 'papier';
-    } else if (argMoveId == 2) {
-    return 'kamień';
-    } else if (argMoveId == 3) {
-    return 'nożyce';
+{
+    const printMessage = function(msg) {
+        let div = document.createElement('div');
+        div.innerHTML = msg;
+        document.getElementById('messages').appendChild(div);
     }
-  
-    printMessage('Nie znam ruchu o id: ' + argMoveId + '.');
-    return 'nieznany ruch';
-}
 
-function gameResult(argComputerMove, argPlayerMove){
-    if( argComputerMove == 'kamień' && argPlayerMove == 'papier' || argComputerMove == 'papier' && argPlayerMove == 'nożyce' || argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
-    return 'won';
-    } else if (argComputerMove == argPlayerMove) {
-    return 'remis';
-    } else {
-    return 'lost';
+    function clearMessages(){
+        document.getElementById('messages').innerHTML = '';
     }
-}
 
-function displayResult(argComputerMove, argPlayerMove, argResult){
-	console.log('Moves: ', argComputerMove, argPlayerMove)
-    printMessage('Komputer wylosował: ' + argComputerMove + ', gracz wybrał: ' + argPlayerMove);
-  
-    if( argResult == 'won'){
-        printMessage('Wygrywasz!');
-    } else if (argResult == 'lost') {
-        printMessage('Przegrywasz :(')
-    } else {
-        printMessage('Remis')
+    function printResult(result){
+        let div = document.createElement('div');
+        div.innerHTML = result;
+        document.getElementById('result').appendChild(div);
+    }
+
+    function clearResult(){
+        document.getElementById('result').innerHTML = '';
+    }
+
+    function getMoveName(argMoveId){
+        if(argMoveId == 1){
+        return 'papier';
+        } else if (argMoveId == 2) {
+        return 'kamień';
+        } else if (argMoveId == 3) {
+        return 'nożyce';
+        }
+    
+        printMessage('Nie znam ruchu o id: ' + argMoveId + '.');
+        return 'nieznany ruch';
+    }
+
+    function gameResult(argComputerMove, argPlayerMove){
+        if( argComputerMove == 'kamień' && argPlayerMove == 'papier' || argComputerMove == 'papier' && argPlayerMove == 'nożyce' || argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+        return 'won';
+        } else if (argComputerMove == argPlayerMove) {
+        return 'remis';
+        } else {
+        return 'lost';
+        }
+    }
+
+    function displayResult(argComputerMove, argPlayerMove, argResult){
+        console.log('Moves: komputer: '+ argComputerMove + ' gracz: ' + argPlayerMove)
+        console.log(gameResult(argComputerMove, argPlayerMove))
+        printMessage('Komputer wylosował: ' + argComputerMove + ', gracz wybrał: ' + argPlayerMove);
+    
+        if( argResult == 'won'){
+            printMessage('Wygrywasz!');
+        } else if (argResult == 'lost') {
+            printMessage('Przegrywasz :(')
+        } else {
+            printMessage('Remis')
+        }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,40 +1,60 @@
-let computerWins = 0;
-let playerWins = 0;
+{
+    let computerWins = 0;
+    let playerWins = 0;
 
-function playGame(arg){
-    clearMessages()
-    clearResult()
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    let computerMove = getMoveName(randomNumber);
-    let playerInput = arg;
-    let playerMove = getMoveName(playerInput);
-    let result = gameResult(computerMove, playerMove);
-    displayResult(computerMove, playerMove, result);
-    
-    if (result == 'won'){
-      playerWins++;
-    } else if (result == 'lost'){
-      computerWins++;
+    const playGame = function (arg) {
+        clearMessages()
+        clearResult()
+        const randomNumber = Math.floor(Math.random() * 3 + 1);
+        let computerMove = getMoveName(randomNumber);
+        const playerInput = arg;
+        const playerMove = getMoveName(playerInput);
+        let result = gameResult(computerMove, playerMove);
+
+        if (result == 'won'){
+          playerWins++;
+        } else if (result == 'lost'){
+          computerWins++;
+        }
+        
+        /*
+
+        if (result == 'lost' && playerWins / (computerWins + playerWins)  < 0.76){
+          console.log('over 75%');
+          computerMove = playerMove;
+          result = 'remis';
+        } else if (result == 'won'){
+          playerWins++;
+        } else if (result == 'lost'){
+          computerWins++;
+        }
+
+        */
+
+        displayResult(computerMove, playerMove, result);
+        printResult('Komputer: ' + computerWins + ' vs ' + 'Gracz: ' + playerWins)
     }
 
-    printResult('Komputer: ' + computerWins + ' vs ' + 'Gracz: ' + playerWins)
+    document.getElementById('play-paper').addEventListener('click', function(){
+        playGame(1);
+      });
+
+    document.getElementById('play-rock').addEventListener('click', function(){
+        playGame(2);
+      });
+
+    document.getElementById('play-scissors').addEventListener('click', function(){
+        playGame(3);
+      });
+
+    /*
+
+    // Test win rate
+
+    for( let i = 0; i < 100; i++ ){
+        playGame(Math.floor(Math.random() * 3 + 1));
+    }
+    
+    */
+
 }
-
-document.getElementById('play-paper').addEventListener('click', function(){
-    playGame(1);
-  });
-
-document.getElementById('play-rock').addEventListener('click', function(){
-    playGame(2);
-  });
-
-document.getElementById('play-scissors').addEventListener('click', function(){
-    playGame(3);
-  });
-  
-  /* 
-
-  Dlaczego ten kod wywołuje się sam po odświezeniu strony, bez kliknięcia?
-  document.getElementById('play-paper').addEventListener('click', playGame(1)); 
-
-  */
